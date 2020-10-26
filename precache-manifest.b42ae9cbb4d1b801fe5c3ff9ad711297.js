727d5a86b4ac50a2ad3ddf8e82b101a62cb84f54
Yashwin12 <munsadwalayashwin@gmail.com>
2020-10-26 14:02:42 -0400

    Updates

---
+++ precache-manifest.b42ae9cbb4d1b801fe5c3ff9ad711297.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca5e1e532b4d459260d2961aebe31301",
+    "url": "/journey-weather/index.html"
+  },
+  {
+    "revision": "c5f67bd51399aaed451b",
+    "url": "/journey-weather/static/css/main.88824ec2.chunk.css"
+  },
+  {
+    "revision": "37f625b0c962763d4e1d",
+    "url": "/journey-weather/static/js/2.f1e53209.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/journey-weather/static/js/2.f1e53209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5f67bd51399aaed451b",
+    "url": "/journey-weather/static/js/main.a2e22991.chunk.js"
+  },
+  {
+    "revision": "6c3c73433d75fca86415",
+    "url": "/journey-weather/static/js/runtime-main.405ee4be.js"
+  }
+]);
